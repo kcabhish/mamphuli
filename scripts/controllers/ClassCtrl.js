c125b4ced286119361db1907199e7a888f48b87f
Abhishek Kc <kcabhish@gmail.com>
2016-08-18 19:57:15 -0400

    Titles change dynamically when page changes

--- scripts/controllers/ClassCtrl.js
+++ scripts/controllers/ClassCtrl.js
@@ -1,3 +1,4 @@
-angular.module("cubicApp").controller("ClassCtrl",['$scope','navBarService',function($scope,navBarService){
+angular.module("cubicApp").controller("ClassCtrl",['$scope','navBarService','staticService',function($scope,navBarService,staticService){
     $scope.classDashboardItems = navBarService.classDashboardItems;
+    staticService.header = "Class";
 }]);
--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,4 +1,9 @@
 angular.module("cubicApp").controller("MainCtrl",['$scope','navBarService','staticService',function($scope,navBarService,staticService){   
     $scope.menuItems = navBarService.menuBarItems;
     $scope.title = staticService.header;
+    $scope.$watch(function(){
+        return staticService.header;
+    },function(){
+        $scope.title = staticService.header;
+    })
 }]);
