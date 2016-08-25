5535e6e18f004fccd3b171420ffc2aa3e71877c6
Abhishek Kc <kcabhish@gmail.com>
2016-08-25 17:45:37 -0400

    Fixing link values

--- src/application/js/service/applicationService.js
+++ src/application/js/service/applicationService.js
@@ -2,19 +2,19 @@ angular.module("cubicApp").service("applicationService",function(){
    this.applicationList =[
         {
             'name':'Home',
-            'path':'views#!/home',
+            'path':'#!/home',
             'imagepath': '/static_files/svg/home.svg',
             'previlege':[]
         },
         {
             'name':'Recruiting Sites',
-            'path':'views#!/application/recruitingSites',
+            'path':'#!/application/recruitingsites',
             'imagepath': '/static_files/svg/domain-registration.svg',
             'previlege':[]
         },
         {
             'name':'Statistics',
-            'path':'views#!/application/statistics',
+            'path':'#!/application/statistics',
             'imagepath': '/static_files/svg/stats.svg',
             'previlege':[]
         }
