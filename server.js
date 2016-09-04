ac3183e7908dad9ef611d08d256cfe945b72feec
Abhishek Kc <kcabhish@gmail.com>
2016-09-03 22:39:45 -0400

    Added service for get requests for company table

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var express = require('express');
+var mysql = require("mysql");
+var connection = require("express-myconnection");
+
 var app = express();
+
+// Create Sql Connection
+app.use(connection(mysql, {
+    host     : 'localhost',
+    user     : 'cubic',
+    password : 'cubictech123',
+    database : 'cms'
+},'request'));
+
 
 app.use(express.static(__dirname + '/'));
 
@@ -8,7 +20,42 @@ app.get('/', function (req, res) {
     res.redirect('/src/main/index.html');
 });
 
-app.get('/landing',function (req, res){
+//Service to get all the companies.
+app.get("/service/companies",function(req,res,next){
+   req.getConnection(function(err, connection) {
+      if (err) return next(err);
+      
+      connection.query('SELECT * FROM companytbl', [], function(err, results) {
+        if (err){
+          console.log(err);
+          return next("Mysql error, check your query");  
+        }         
+        res.json(results);
+      });
+      
+    });
+    
+});
+
+//Service to get the company information based upon the id passed
+app.get("/service/companies/:companyid",function(req,res,next){
+    var companyId = req.params.companyid;
+   req.getConnection(function(err, connection) {
+      if (err) return next(err);
+      
+      connection.query('SELECT * FROM companytbl where companyid = ?',[companyId], function(err, results) {
+        if (err){
+          console.log(err);
+          return next("Mysql error, check your query");  
+        }         
+        res.json(results);
+      });
+      
+    });
+    
+});
+
+app.get('/landing',function (req, res, next){
     res.send('In landing page');
 });
 
