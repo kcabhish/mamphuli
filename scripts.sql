9c2f695644f44daee7ee57a5c70e3a0afa09f9af
Abhishek Kc <kcabhish@gmail.com>
2016-10-30 12:49:15 -0400

    Modified SQL queries

--- scripts.sql
+++ scripts.sql
@@ -12,22 +12,12 @@ CHANGE COLUMN `active` `active` INT(1) NULL DEFAULT 1 ,
 ADD UNIQUE INDEX `companyid_UNIQUE` (`companyid` ASC);
 
 
-INSERT INTO cms.companytbl (companyname) VALUE ("Cubic");
-INSERT INTO cms.companytbl (companyname) VALUE ("Breign");
-
-
 CREATE TABLE `cms`.`classtbl` (
   `classid` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(45) NOT NULL,
   `active` INT(1) NULL DEFAULT 1,
   PRIMARY KEY (`classid`),
   UNIQUE INDEX `classid_UNIQUE` (`classid` ASC));
-  
-INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Basic Java');
-INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Advanced Java');
-INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Angular JS');
-INSERT INTO `cms`.`classtbl` (`description`) VALUES ('.NET');
-INSERT INTO `cms`.`classtbl` (`description`) VALUES ('VM Ware');
 
 CREATE TABLE `cms`.`categorytypetbl` (
   `categorytypeid` INT NOT NULL AUTO_INCREMENT,
@@ -36,17 +26,6 @@ CREATE TABLE `cms`.`categorytypetbl` (
   PRIMARY KEY (`categorytypeid`),
   UNIQUE INDEX `categorytypeid_UNIQUE` (`categorytypeid` ASC));
 
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('System Admin');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('CEO');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('COO');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('CTO');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('HR');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('VC');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Instructor');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Call Taker');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Sales');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Applier');
-INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Recruiter');
 
 CREATE TABLE `cms`.`employeetbl` (
   `employeeid` INT NOT NULL AUTO_INCREMENT,
@@ -66,9 +45,6 @@ CREATE TABLE `cms`.`employeetbl` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
     
-INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `address`, `phone`, `email`, `companyid`, `active`) VALUES ('Abhishek', 'KC', '4248 Club house Circle', '413-345-1499', 'kcabhish@gmail.com', '1', '1');
-INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Biplab', 'Wagle', '214-730-3703', 'bob@cubicit.net', '1', '1');
-INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Manan', 'Sapkota', '510-717-1286', 'manan@breign.net', '2', '1');
 
 CREATE TABLE `cms`.`sessiontbl` (
   `sessionid` INT NOT NULL AUTO_INCREMENT,
@@ -99,9 +75,6 @@ ADD CONSTRAINT `instructor`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-INSERT INTO `cms`.`sessiontbl` (`name`, `classid`, `startdate`, `enddate`, `starttime`, `endtime`, `capacity`, `active`, `instructor`) VALUES ('UIKC082016', '3', '2016-08-02', '2016-09-15', '10am', '12pm', '20', '1', '1');
-INSERT INTO `cms`.`sessiontbl` (`name`, `classid`, `startdate`, `enddate`, `starttime`, `endtime`, `capacity`, `active`, `instructor`) VALUES ('JAVAKC082016', '2', '2016-08-01', '2016-09-15', '5pm', '7pm', '20', '1', '2');
-
 CREATE TABLE `cms`.`logintbl` (
   `loginid` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
@@ -117,11 +90,6 @@ CREATE TABLE `cms`.`logintbl` (
     ON UPDATE CASCADE);
 ALTER TABLE `cms`.`logintbl` 
 ADD UNIQUE INDEX `employeeid_UNIQUE` (`employeeid` ASC);
-
-
-INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('avsek007', '1234', '1');
-INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('bob007', '1234', '2');
-INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('manan', '1234', '3');
 
 
 CREATE TABLE `cms`.`employeecategorytbl` (
@@ -140,12 +108,7 @@ CREATE TABLE `cms`.`employeecategorytbl` (
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '1');
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '4');
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '9');
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('2', '1');
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('2', '6');
-INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '7');
+
 
 CREATE TABLE `cms`.`educationtbl` (
   `educationid` INT NOT NULL AUTO_INCREMENT,
@@ -153,10 +116,6 @@ CREATE TABLE `cms`.`educationtbl` (
   PRIMARY KEY (`educationid`),
   UNIQUE INDEX `educationid_UNIQUE` (`educationid` ASC));
 
-INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('High School');
-INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Bachelors');
-INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Masters');
-INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Phd');
 
 
 CREATE TABLE `cms`.`workstatustbl` (
@@ -166,12 +125,6 @@ CREATE TABLE `cms`.`workstatustbl` (
   PRIMARY KEY (`statusid`),
   UNIQUE INDEX `statusid_UNIQUE` (`statusid` ASC));
 
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('US Citizen');
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('Green Card');
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('Work Permit');
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(Green Card)');
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(OPT)');
-INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(CPT)');
 
 CREATE TABLE `cms`.`consultantstatustbl` (
   `statusid` INT NOT NULL AUTO_INCREMENT,
@@ -179,11 +132,6 @@ CREATE TABLE `cms`.`consultantstatustbl` (
   PRIMARY KEY (`statusid`),
   UNIQUE INDEX `statusid_UNIQUE` (`statusid` ASC));
 
-INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Not Recruited');
-INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Recruited');
-INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Marketing');
-INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Placed');
-INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Benched');
 
 CREATE TABLE `cms`.`consultantstbl` (
   `consultantsid` INT NOT NULL AUTO_INCREMENT,
@@ -225,10 +173,7 @@ CREATE TABLE `cms`.`consultantstbl` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO `cms`.`consultantstbl` (`firstname`, `middlename`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Rubin', '', 'Buddhacharya', '123-456-7891', 'rubin@gmail.com', '1234', 'Irving', '56456', '2012-12-12', '123-32-3214', '3', '#asdf1234', 'ND', '3', 'rubin007', '2', '1');
 
-INSERT INTO `cms`.`consultantstbl` (`firstname`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Anish', 'KC', '123-123-1234', 'anish.kc@gmail.com', '1234', 'Irving', '12345', '1985-12-29', '123-12-1233', '4', '#asdfasdf', 'MD', '3', 'anish007', '3', '1');
-INSERT INTO `cms`.`consultantstbl` (`firstname`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Drona', 'Baral', '123-123-1233', 'drona@gmail.com', '1234', 'Dallas', '54678', '1982-12-31', '123-34-1234', '1', '#asdffdasf', 'MN', '3', 'drona123', '1', '1');
 
 CREATE TABLE `cms`.`consultanteducationtbl` (
   `consultantid` INT NOT NULL,
@@ -310,4 +255,73 @@ CREATE TABLE `cms`.`marketingtbl` (
     ON UPDATE NO ACTION);
 
 
+#Inserts below here
 
+INSERT INTO cms.companytbl (companyname) VALUE ("Cubic");
+INSERT INTO cms.companytbl (companyname) VALUE ("Breign");
+
+
+INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Basic Java');
+INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Advanced Java');
+INSERT INTO `cms`.`classtbl` (`description`) VALUES ('Angular JS');
+INSERT INTO `cms`.`classtbl` (`description`) VALUES ('.NET');
+INSERT INTO `cms`.`classtbl` (`description`) VALUES ('VM Ware');
+
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('System Admin');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('CEO');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('COO');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('CTO');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('HR');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('VC');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Instructor');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Call Taker');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Sales');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Applier');
+INSERT INTO `cms`.`categorytypetbl` (`description`) VALUES ('Recruiter');
+
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `address`, `phone`, `email`, `companyid`, `active`) VALUES ('Abhishek', 'KC', '4248 Club house Circle', '413-345-1499', 'ui@cubicit.net', '1', '1');
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Biplab', 'Wagle', '214-730-3703', 'bob@cubicit.net', '1', '1');
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Ramitha', 'Khadka', '214-730-3703', 'ramita007@gmail.com', '1', '1');
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Subash', 'Khadka', '214-499-4294', 'sbshkhadka@gmail.com', '1', '1');
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Nabin', 'Koirala', '682-433-9680', 'koiralabaje@hotmail.com', '1', '1');
+INSERT INTO `cms`.`employeetbl` (`firstname`, `lastname`, `phone`, `email`, `companyid`, `active`) VALUES ('Azeem', 'Marediya', '469-412-1985', 'azeem@cubicit.net', '1', '1');
+
+
+INSERT INTO `cms`.`sessiontbl` (`name`, `classid`, `startdate`, `enddate`, `starttime`, `endtime`, `capacity`, `active`, `instructor`) VALUES ('UIKC082016', '3', '2016-08-02', '2016-09-15', '10am', '12pm', '20', '1', '1');
+INSERT INTO `cms`.`sessiontbl` (`name`, `classid`, `startdate`, `enddate`, `starttime`, `endtime`, `capacity`, `active`, `instructor`) VALUES ('JAVAKC082016', '2', '2016-08-01', '2016-09-15', '5pm', '7pm', '20', '1', '2');
+
+INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('avsek007', '1234', '1');
+INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('bob007', '1234', '2');
+INSERT INTO `cms`.`logintbl` (`username`, `password`, `employeeid`) VALUES ('manan', '1234', '3');
+
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '1');
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '4');
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '9');
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('2', '1');
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('2', '6');
+INSERT INTO `cms`.`employeecategorytbl` (`employeeid`, `categoryid`) VALUES ('1', '7');
+
+
+INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('High School');
+INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Bachelors');
+INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Masters');
+INSERT INTO `cms`.`educationtbl` (`description`) VALUES ('Phd');
+
+
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('US Citizen');
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('Green Card');
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('Work Permit');
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(Green Card)');
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(OPT)');
+INSERT INTO `cms`.`workstatustbl` (`description`) VALUES ('EAD(CPT)');
+
+INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Not Recruited');
+INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Recruited');
+INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Marketing');
+INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Placed');
+INSERT INTO `cms`.`consultantstatustbl` (`description`) VALUES ('Benched');
+
+INSERT INTO `cms`.`consultantstbl` (`firstname`, `middlename`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Rubin', '', 'Buddhacharya', '123-456-7891', 'rubin@gmail.com', '1234', 'Irving', '56456', '2012-12-12', '123-32-3214', '3', '#asdf1234', 'ND', '3', 'rubin007', '2', '1');
+
+INSERT INTO `cms`.`consultantstbl` (`firstname`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Anish', 'KC', '123-123-1234', 'anish.kc@gmail.com', '1234', 'Irving', '12345', '1985-12-29', '123-12-1233', '4', '#asdfasdf', 'MD', '3', 'anish007', '3', '1');
+INSERT INTO `cms`.`consultantstbl` (`firstname`, `lastname`, `phone`, `email`, `address`, `city`, `zip`, `dob`, `ssn`, `workstatus`, `licensenumber`, `licensestate`, `recruiter`, `skypeid`, `consultantstatus`, `active`) VALUES ('Drona', 'Baral', '123-123-1233', 'drona@gmail.com', '1234', 'Dallas', '54678', '1982-12-31', '123-34-1234', '1', '#asdffdasf', 'MN', '3', 'drona123', '1', '1');
